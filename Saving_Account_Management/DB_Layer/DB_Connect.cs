51cdc8397aad0bc61410b30c51b88bb626eb7229
KimLe <kimle020102@gmail.com>
2022-11-18 10:36:08 +0700

    changes

--- Saving_Account_Management/DB_Layer/DB_Connect.cs
+++ Saving_Account_Management/DB_Layer/DB_Connect.cs
@@ -70,24 +70,6 @@ namespace Saving_Account_Management.DB_Layer
             }
             return f;
         }
-      
-       public void myConnect()
-        {
-            Conn.Open();
-        }
-        //combobox sự kiện selectindexchange
-        public void myClose()
-        {
-            Conn.Close();
-        }
-
-        public DataTable createTable(string sql)
-        {
-            DataTable dt = new DataTable();
-            SqlDataAdapter ds = new SqlDataAdapter(sql, Conn);
-            ds.Fill(dt);
-            return dt;
-        }
 
     }
 }
